369932403bdd78b96550c93273b41401b859c567
charvey <charvey@cs.byu.edu>
2025-12-27 21:26:31 -0700

    feat: add LayeredShader, FillLayer, and LayerMask data structures.

--- models.js
+++ models.js
@@ -17,3 +17,23 @@ function NodeConnection(attributeName, sourceNode) {
     this.attributeName = attributeName;
     this.sourceNode = sourceNode;
 }
+
+// Layered Shader Data Structures
+function LayeredShader(baseMaterial) {
+    this.baseMaterial = baseMaterial || null;
+    this.fillLayers = []; // List of FillLayer objects
+}
+
+function FillLayer() {
+    this.masks = []; // List of LayerMask objects
+    this.colorContribution = 1.0;
+    this.heightContribution = 1.0;
+    this.roughnessContribution = 1.0;
+    this.roughness2Contribution = 1.0;
+    this.metallicContribution = 1.0;
+}
+
+function LayerMask() {
+    this.compositionMode = "add";
+    this.percentage = 1.0;
+}
